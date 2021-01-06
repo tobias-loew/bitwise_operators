29024f0fcad4e65a2c165d1609a07acea6bd598b
Tobias Loew <tobias-loew@users.noreply.github.com>
2021-01-06 10:56:54 +0100

    Update bitwise_operators.hpp

--- bitwise_operators.hpp
+++ bitwise_operators.hpp
@@ -5,7 +5,7 @@
 //  Copyright Tobias Loew 2020. Use, modification and
 //  distribution is subject to the Boost Software License, Version
 //  1.0. (See accompanying file LICENSE_1_0.txt or copy at
-//  http://www.lunaticpp.org/LICENSE_1_0.txt)
+//  http://www.boost.org/LICENSE_1_0.txt)
 //
 // For more information, see https://github.com/tobias-loew/bitwise_operators
 //
